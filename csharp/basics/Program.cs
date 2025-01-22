8cb428318fad506f620a2a137fc6cac5c55ce973
Silas <silas.duarte.h@gmail.com>
2025-01-22 08:23:11 -0600

    data types notes

--- csharp/basics/Program.cs
+++ csharp/basics/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-            Console.WriteLine(   "Hello, World!");
+// DATA TYPES
+
+// byte myByte = 1;
+// short myShort = 1;
+// int myInt = 1;
+// long myLong = 1;
+// 
+// float myFloat = 10.5f;
+// double myDouble = 10.5;
+// decimal myDecimal = 10.5m;
+// 
+// bool isBoolean = true;
+// 
+// char aCharacter = 'A';
+string aString = "Hello world!";
+
+Console.WriteLine($"{aString}");
